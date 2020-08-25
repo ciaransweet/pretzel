060812bc83be8bfba9636c765ed2f1b7d0d1dae4
Ciaran Evans <9111975+ciaranevans@users.noreply.github.com>
2020-08-25 20:55:01 +0100

    Finalise logic for collecting decoration descriptions

--- lambdas/collect_decorations/function/decorations_collector.py
+++ lambdas/collect_decorations/function/decorations_collector.py
@@ -10,7 +10,13 @@ decoration_descriptions = {
 }
 
 
-def collect_decorations(order: Dict[str, str]) -> List[str]:
+def _get_decoration_descriptions(order: Dict[str, str]) -> List[str]:
+    """
+    For a given order, return the textual description of the decorations
+    :param order: Dict[str, str] representing an order
+    :returns: List[str] a list containing the textual descriptions of decorations in an
+    order, if there are decorations
+    """
     if "pretzel_decorations" not in order.keys():
         return []
     decorations = [
@@ -18,3 +24,26 @@ def collect_decorations(order: Dict[str, str]) -> List[str]:
         for decoration in order["pretzel_decorations"]
     ]
     return decorations
+
+
+def collect_decoration_descriptions(order: Dict[str, str]) -> str:
+    """
+    Collects the textual descriptions of decorations in an order, message differs
+    whether the order contains decorations or not
+    :param order: Dict[str, str] representing an order
+    :returns: str a textual description of all of the decorations in an order, string
+    returned depends on whether decorations are present or not
+    """
+    decorations = _get_decoration_descriptions(order)
+    if len(decorations) < 3:
+        decoration_description = (
+            "For your pretzels, you've chosen:"
+            f" {' and '.join(decorations)}."
+        )
+    else:
+        decoration_description = (
+            "For your pretzels, you've chosen:"
+            f" {', '.join(decorations[:-1])}"
+            f", and {decorations[-1]}."
+        )
+    return decoration_description
--- tests/unit/lambdas/collect_decorations/test_collect_decorations.py
+++ tests/unit/lambdas/collect_decorations/test_collect_decorations.py
@@ -4,11 +4,15 @@ import os
 from assertpy import assert_that
 
 from lambdas.collect_decorations.function.decorations_collector import (
-    collect_decorations,
+    _get_decoration_descriptions,
+    collect_decoration_descriptions,
 )
 
-EXAMPLE_ORDER_JSON_PATH = (
-    f"{os.path.dirname(os.path.abspath(__file__))}/example_order.json"
+EXAMPLE_FULL_ORDER_JSON_PATH = (
+    f"{os.path.dirname(os.path.abspath(__file__))}/full_example_order.json"
+)
+EXAMPLE_PARTIAL_ORDER_JSON_PATH = (
+    f"{os.path.dirname(os.path.abspath(__file__))}/partial_example_order.json"
 )
 
 
@@ -21,14 +25,40 @@ def test_that_decorations_descriptions_returned_correctly():
         "a tickle of salt pieces",
         "a sticky drizzle of honey",
     ]
-    with open(EXAMPLE_ORDER_JSON_PATH, "r") as order_in:
+    with open(EXAMPLE_FULL_ORDER_JSON_PATH, "r") as order_in:
         order_json = json.load(order_in)
-    decorations = collect_decorations(order_json)
+    decorations = _get_decoration_descriptions(order_json)
     assert_that(decorations).contains_only(*expected_decorations)
 
 
 def test_that_no_decorations_returns_empty_list():
-    decorations = collect_decorations({"pretzel_decorations": []})
+    decorations = _get_decoration_descriptions({"pretzel_decorations": []})
     assert_that(decorations).is_empty()
-    decorations = collect_decorations({})
+    decorations = _get_decoration_descriptions({})
     assert_that(decorations).is_empty()
+
+
+def test_that_less_than_three_decorations_returns_correct_description():
+    expected_description = (
+        "For your pretzels, you've chosen: a sweet, sticky caramel"
+        " sauce and a smooth, creamy chocolate sauce."
+    )
+    with open(EXAMPLE_PARTIAL_ORDER_JSON_PATH, "r") as order_in:
+        order_json = json.load(order_in)
+    description = collect_decoration_descriptions(order_json)
+    assert_that(description).is_equal_to(expected_description)
+
+
+def test_that_decorations_returns_correct_description():
+    expected_description = (
+        "For your pretzels, you've chosen: a sweet, sticky caramel sauce, "
+        "a smooth, creamy chocolate sauce, "
+        "a dashing of colourful sugar sprinkles, "
+        "a sprinkling of crunchy sesame seeds, "
+        "a tickle of salt pieces, and "
+        "a sticky drizzle of honey."
+    )
+    with open(EXAMPLE_FULL_ORDER_JSON_PATH, "r") as order_in:
+        order_json = json.load(order_in)
+    description = collect_decoration_descriptions(order_json)
+    assert_that(description).is_equal_to(expected_description)
